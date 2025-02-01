144702b6e23fce3a10b8c5fb0b046061350e1c0d
talhabutt7 <talhabuttt@gmail.com>
2025-02-01 20:22:27 +0000

    Explicit SRID

--- db/migrate/20250201175503_create_green_sites.rb
+++ db/migrate/20250201175503_create_green_sites.rb
@@ -4,7 +4,7 @@ class CreateGreenSites < ActiveRecord::Migration[7.1]
 
     create_table :green_sites do |t|
       t.string :name, null: false
-      t.st_point :coordinates, geographic: true
+      t.st_point :coordinates, geographic: true, srid: 4326 # Explicit SRID
       t.string :taxonomy_code
       t.timestamps
     end
