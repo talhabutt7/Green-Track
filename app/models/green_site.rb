8dbe9cd16820219915121bec028abc0c53f39de0
talhabutt7 <talhabuttt@gmail.com>
2025-02-01 20:23:09 +0000

    within_radius scope added

--- app/models/green_site.rb
+++ app/models/green_site.rb
@@ -1,2 +1,13 @@
+# app/models/green_site.rb
 class GreenSite < ApplicationRecord
+  scope :within_radius, ->(lng, lat, radius_km) {
+    where(
+      "ST_DWithin(
+        ST_SetSRID(coordinates::geometry, 4326)::geography,
+        ST_SetSRID(ST_MakePoint(:lng, :lat), 4326)::geography,
+        :radius
+      )",
+      lng: lng, lat: lat, radius: radius_km * 1000
+    )
+  }
 end
