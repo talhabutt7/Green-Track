e755445151290960c67041c35f1a4fabc6ea20a0
talhabutt7 <talhabuttt@gmail.com>
2025-02-01 18:02:52 +0000

    Authentication functionality

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,14 @@
+# app/controllers/api/v1/authentication_controller.rb
+module Api::V1
+  class AuthenticationController < ApplicationController
+    def login
+      user = User.find_by_email(params[:email])
+      if user&.valid_password?(params[:password])
+        token = user.generate_jwt
+        render json: { token: token }
+      else
+        render json: { error: 'Invalid credentials' }, status: :unauthorized
+      end
+    end
+  end
+end
